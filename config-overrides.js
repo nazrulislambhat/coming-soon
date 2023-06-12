f57a323f8f12ddeb05962b70e4eb7e3552da26d0
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-12 13:34:57 +0530

    Initial

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = function override(config) {
+  config.module.rules.push({
+    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    include: path.resolve(__dirname, 'src/fonts'),
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/',
+        },
+      },
+    ],
+  });
+
+  return config;
+};
