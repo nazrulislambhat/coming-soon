33b7f4333acb8e9fe6dc01a610802b4f0de7380e
Nazrul Islam <nazrulislambhat@gmail.com>
2023-12-31 10:27:33 +0530

    Initial Commit

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ComingSoon from './ComingSoon';
-
-function App() {
-  return <ComingSoon />;
-}
-
-export default App;
--- tailwind.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // ...
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-};
